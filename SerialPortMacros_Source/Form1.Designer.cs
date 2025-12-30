6635c8901cb21be501f7fce1c97a2b8e0e60150e
Agasa's Workshop <142116808+Agasaworkshop@users.noreply.github.com>
2025-12-30 21:17:47 +0100

    Add files via upload

---
+++ SerialPortMacros_Source/Form1.Designer.cs
@@ -0,0 +1,524 @@
+﻿namespace SerialPortMacros
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            textBox6 = new TextBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            textBox2 = new RichTextBox();
+            button12 = new Button();
+            button13 = new Button();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox8 = new CheckBox();
+            button14 = new Button();
+            checkBox9 = new CheckBox();
+            button15 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(694, 404);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(694, 369);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Macros";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 336);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(620, 27);
+            textBox1.TabIndex = 2;
+            textBox1.KeyPress += textBox1_Keypress;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(74, 372);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(56, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button3.BackColor = Color.White;
+            button3.Image = Properties.Resources.AddConnection;
+            button3.Location = new Point(126, 404);
+            button3.Name = "button3";
+            button3.Size = new Size(27, 29);
+            button3.TabIndex = 5;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(407, 405);
+            button4.Name = "button4";
+            button4.Size = new Size(27, 29);
+            button4.TabIndex = 7;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(351, 372);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(57, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(206, 372);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(62, 27);
+            textBox5.TabIndex = 6;
+            // 
+            // button5
+            // 
+            button5.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(265, 405);
+            button5.Name = "button5";
+            button5.Size = new Size(27, 29);
+            button5.TabIndex = 7;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button6.Image = Properties.Resources.AddConnection;
+            button6.Location = new Point(545, 405);
+            button6.Name = "button6";
+            button6.Size = new Size(27, 29);
+            button6.TabIndex = 9;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(490, 373);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(56, 27);
+            textBox6.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(59, 405);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(71, 28);
+            comboBox2.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(197, 405);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(71, 28);
+            comboBox1.TabIndex = 12;
+            // 
+            // comboBox3
+            // 
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(337, 405);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(71, 28);
+            comboBox3.TabIndex = 13;
+            // 
+            // comboBox4
+            // 
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(475, 405);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(71, 28);
+            comboBox4.TabIndex = 14;
+            comboBox4.SelectedIndexChanged += comboBox4_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 376);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 20);
+            label1.TabIndex = 15;
+            label1.Text = "baud";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(159, 376);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 20);
+            label2.TabIndex = 16;
+            label2.Text = "baud";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(307, 376);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 20);
+            label3.TabIndex = 17;
+            label3.Text = "baud";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(447, 375);
+            label4.Name = "label4";
+            label4.Size = new Size(43, 20);
+            label4.TabIndex = 18;
+            label4.Text = "baud";
+            // 
+            // button7
+            // 
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.Location = new Point(578, 391);
+            button7.Name = "button7";
+            button7.Size = new Size(28, 29);
+            button7.TabIndex = 19;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Image = Properties.Resources.Settings_red;
+            button8.Location = new Point(126, 372);
+            button8.Name = "button8";
+            button8.Size = new Size(27, 29);
+            button8.TabIndex = 20;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Image = Properties.Resources.Settings_blue;
+            button9.Location = new Point(265, 372);
+            button9.Name = "button9";
+            button9.Size = new Size(27, 29);
+            button9.TabIndex = 21;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Image = Properties.Resources.Settings_yellow;
+            button10.Location = new Point(407, 371);
+            button10.Name = "button10";
+            button10.Size = new Size(27, 29);
+            button10.TabIndex = 22;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Image = Properties.Resources.Settings_green;
+            button11.Location = new Point(545, 372);
+            button11.Name = "button11";
+            button11.Size = new Size(27, 29);
+            button11.TabIndex = 23;
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(32, 411);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(18, 17);
+            checkBox1.TabIndex = 24;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(173, 411);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(18, 17);
+            checkBox2.TabIndex = 25;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(313, 412);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(18, 17);
+            checkBox3.TabIndex = 26;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(451, 412);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(18, 17);
+            checkBox4.TabIndex = 27;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.White;
+            textBox2.Location = new Point(12, 12);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(776, 318);
+            textBox2.TabIndex = 28;
+            textBox2.Text = "";
+            // 
+            // button12
+            // 
+            button12.Image = Properties.Resources.Lock;
+            button12.Location = new Point(697, 335);
+            button12.Name = "button12";
+            button12.Size = new Size(30, 29);
+            button12.TabIndex = 29;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Image = Properties.Resources.Eraser;
+            button13.Location = new Point(728, 335);
+            button13.Name = "button13";
+            button13.Size = new Size(30, 29);
+            button13.TabIndex = 30;
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(490, 445);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(53, 24);
+            checkBox5.TabIndex = 31;
+            checkBox5.Text = "log";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(351, 445);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(53, 24);
+            checkBox6.TabIndex = 32;
+            checkBox6.Text = "log";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(206, 445);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(53, 24);
+            checkBox7.TabIndex = 33;
+            checkBox7.Text = "log";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(74, 445);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(53, 24);
+            checkBox8.TabIndex = 34;
+            checkBox8.Text = "log";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(678, 439);
+            button14.Name = "button14";
+            button14.Size = new Size(110, 29);
+            button14.TabIndex = 35;
+            button14.Text = "Start Logging";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(638, 338);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(53, 24);
+            checkBox9.TabIndex = 36;
+            checkBox9.Text = "log";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Image = Properties.Resources.Time_color_16x;
+            button15.Location = new Point(759, 335);
+            button15.Name = "button15";
+            button15.Size = new Size(30, 29);
+            button15.TabIndex = 37;
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 487);
+            Controls.Add(button15);
+            Controls.Add(checkBox9);
+            Controls.Add(button14);
+            Controls.Add(checkBox8);
+            Controls.Add(checkBox7);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox5);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(textBox2);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(button6);
+            Controls.Add(textBox6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(button3);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "SerialPortMacros";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox6;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private RichTextBox textBox2;
+        private Button button12;
+        private Button button13;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private Button button14;
+        private CheckBox checkBox9;
+        private Button button15;
+    }
+}
---
+++ SerialPortMacros_Source/Form2.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SerialPortMacros
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            databitsbox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            Paritybox = new ComboBox();
+            stopbitsbox = new ComboBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 198);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(120, 198);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Apply";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Parity";
+            // 
+            // databitsbox
+            // 
+            databitsbox.Location = new Point(89, 56);
+            databitsbox.Name = "databitsbox";
+            databitsbox.Size = new Size(125, 27);
+            databitsbox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Data bits";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 117);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Stop bits";
+            // 
+            // Paritybox
+            // 
+            Paritybox.FormattingEnabled = true;
+            Paritybox.Location = new Point(89, 6);
+            Paritybox.Name = "Paritybox";
+            Paritybox.Size = new Size(125, 28);
+            Paritybox.TabIndex = 11;
+            // 
+            // stopbitsbox
+            // 
+            stopbitsbox.FormattingEnabled = true;
+            stopbitsbox.Location = new Point(89, 117);
+            stopbitsbox.Name = "stopbitsbox";
+            stopbitsbox.Size = new Size(125, 28);
+            stopbitsbox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 20);
+            label4.TabIndex = 13;
+            label4.Text = "Custom Buffer";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 162);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(82, 27);
+            textBox1.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(200, 165);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(18, 17);
+            checkBox1.TabIndex = 15;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(226, 239);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(stopbitsbox);
+            Controls.Add(Paritybox);
+            Controls.Add(label3);
+            Controls.Add(databitsbox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            MaximumSize = new Size(244, 286);
+            MinimizeBox = false;
+            MinimumSize = new Size(244, 256);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        public TextBox databitsbox;
+        private Label label2;
+        private Label label3;
+        public ComboBox Paritybox;
+        public ComboBox stopbitsbox;
+        private Label label4;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+    }
+}
